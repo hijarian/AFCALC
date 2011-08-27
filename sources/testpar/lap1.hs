f399566154d6dc372f93efd590d912ef83ee74ec
Saphronov Mark <hijarian@gmail.com>
2011-08-27 11:10:37 +0400

    Commit to make sure the source/ subdirectory really is deleted

--- sources/testpar/lap1.hs
+++
@@ -1,8 +0,0 @@
-import GHC.Conc
-import System.Info
-import Text.Printf
-import Data.Version
-
-main = do
-  printf "Compiled with %s-%s on %s/%s\n" compilerName (showVersion compilerVersion) os arch 
-  printf "Running with %d OS threads\n" numCapabilities 
