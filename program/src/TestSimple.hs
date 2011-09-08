5ccd1cd4284e4ab1f8d35e71ef9d0fa258e4347a
Saphronov Mark <hijarian@gmail.com>
2011-09-09 01:49:30 +0400

    Fixed nasty bug with non-synchronized tau TODO: somehow make tau be defined in one place only

--- program/src/TestSimple.hs
+++ program/src/TestSimple.hs
@@ -41,30 +41,33 @@ extract_param_names param =
   let phi0   = show $ phi_0 param
       v0     = show $ v_0 param
       alpha0 = show $ alpha param
-      a0     = show $ a param
-      b0     = show $ b param
       abstau = show $ BlastModel.Simple.tau param
-      precision0 = show $ precision param
-  in "Phi0 = " ++ phi0 ++ ", V0 = " ++ v0 ++ ", Alpha0 = " ++ alpha0 ++ ", A = " ++ a0 ++ ", B = " ++ b0 ++ ", tau = " ++ abstau ++ ", precision = " ++ precision0
+  in "Phi0 = " ++ phi0 ++ ", V0 = " ++ v0 ++ ", Alpha0 = " ++ alpha0 ++ ", tau = " ++ abstau
 
 --------------------------------------------------------------------------------
 
 -- Default tau
 tau' = 0.6
 
+-- Default model parameters
+model_params = null_parameters{
+    BlastModel.Simple.tau = tau',
+    alpha = 0.3
+  }
+
 -- Default calc parameters
 calc_params = CalcParams{
   -- Points are A, B, C and D
-  points = (((pi/4) :+ (pi*tau'/4)), (0 :+ (pi*tau'/4)), (0 :+ 0), ((pi/4) :+ 0)),
+  points = (((pi/4)*(0.99) :+ (pi*tau'/4)), (0 :+ (pi*tau'/4)), (0 :+ 0), ((pi/4)*(0.99) :+ 0)),
   AFCalc.tau = tau',
   n_integral = 20
   }
 
 -- Program entry point with tests
 main = do
-  let param = calc_params{AFCalc.dzdu = (BlastModel.Simple.dzdu null_parameters)}
+  let param = calc_params{AFCalc.dzdu = (BlastModel.Simple.dzdu model_params)}
   datalist <- calcPoints param
-  let linetitle = extract_param_names null_parameters
+  let linetitle = extract_param_names model_params
   plotArea linetitle datalist
   pngArea ("../img/" ++ linetitle ++ ".png") linetitle datalist
   return ()
